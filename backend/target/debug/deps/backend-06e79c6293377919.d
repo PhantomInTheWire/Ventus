c10525242ee947a141ba0c0e5fb9eeb302c873fc
Karan <karanlokchandani@protonmail.com>
2024-10-12 19:30:19 +0530

    fix ishu-codes mistake

--- backend/target/debug/deps/backend-06e79c6293377919.d
+++
@@ -1,5 +0,0 @@
-D:\Web\ReactNative\Ventus\backend\target\debug\deps\libbackend-06e79c6293377919.rmeta: src\main.rs
-
-D:\Web\ReactNative\Ventus\backend\target\debug\deps\backend-06e79c6293377919.d: src\main.rs
-
-src\main.rs:
--- backend/target/debug/deps/backend-dcc9c4f6a7cc5e43.d
+++
@@ -1,5 +0,0 @@
-D:\Web\ReactNative\Ventus\backend\target\debug\deps\libbackend-dcc9c4f6a7cc5e43.rmeta: src\main.rs
-
-D:\Web\ReactNative\Ventus\backend\target\debug\deps\backend-dcc9c4f6a7cc5e43.d: src\main.rs
-
-src\main.rs:
